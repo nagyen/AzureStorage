45d95b6dae212284ca150937580666f32b9fcea7
Nagarjuna Yendluri <nag.yendluri09@gmail.com>
2017-08-08 21:33:36 -0400

    Added tests and core projects

---
+++ core/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace core.Models
+{
+    public class User : TableEntity
+    {
+        public User(string lastName, string firstName)
+        {
+            this.PartitionKey = lastName;
+            this.RowKey = firstName;
+        }
+        
+        public User() { }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+        
+        public string Image { get; set; }
+    }
+}
---
+++ core/Services/UserService.cs
@@ -0,0 +1,7 @@
+﻿namespace core.Services
+{
+    public class UserService
+    {
+        
+    }
+}
