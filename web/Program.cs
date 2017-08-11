0cc72d9d59214edab088072991d4625f97ffa455
Nagarjuna Yendluri <nag.yendluri09@gmail.com>
2017-08-10 21:45:30 -0400

    Added docker build files

--- web/Program.cs
+++ web/Program.cs
@@ -15,6 +15,7 @@ namespace web
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
 
